92868f60def19599faafcffb3395808b47259052
mariasalud <86627563+mariasalud@users.noreply.github.com>
2021-12-01 11:21:31 -0600

    Added the search engine.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.png';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
        <img src={logo} className="App-logo" alt="logo" />
      </header>
-    </div>
+     <main>
+       <Dictionary />
+       </main>
+     <footer className="App-footer">
+       <small>Coded by Maria Garcia</small>
+     </footer>
+     </div>
+     </div>
   );
 }
 
