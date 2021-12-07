9045c51a38a2c8268bb042c0afe4a3d36c1757dd
mariasalud <86627563+mariasalud@users.noreply.github.com>
2021-12-07 09:09:42 -0600

    Intergraded the synonyms component.

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,17 +1,21 @@
 import React from "react";
+import Synonyms from "./Synonyms"; 
 
 export default function Meaning(props) {
+  console.log(props.meaning);
     return (
-      <div className="meaning">
+      <div className="Meaning">
         <h3>{props.meaning.partOfSpeech}</h3>
         {props.meaning.definitions.map(function (definition, index) {
          return (
           <div key={index}>
                <p>
-                {definition.definition}
+                <strong>Definition:</strong> {definition.definition}
                   <br />
+                <strong>Example:</strong>
                 <em>{definition.example}</em>
-              </p>
+                <Synonyms synonyms={definition.synonyms} />
+                </p>
             </div>
           );
         })}
