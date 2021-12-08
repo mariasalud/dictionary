bacd7add73186a939f6d44ee0c5e17b325a0d956
mariasalud <86627563+mariasalud@users.noreply.github.com>
2021-12-08 08:25:34 -0600

    Added the rel noreferrer.

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,11 +3,11 @@ import React from "react";
 export default function Phonetic(props) {
     return (
      <div className="Phonetic">
-         <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+         <a href={props.phonetic.audio}  rel="noreferrer" target="_blank">
              Listen
          </a>
          <br />
          {props.phonetic.text}
      </div>
-    );
+ );
 }
